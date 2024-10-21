d105cd423b7530ff05098a04b913cf8bba70ecb7
Christian Kim <christiansactualbusiness@gmail.com>
2024-10-20 21:10:54 -0700

    Add files via upload

---
+++ assets/js/main.js
@@ -0,0 +1,7 @@
+// To be used for general functionality of the webpage
+$( document ).ready(function( ) {
+
+  // Just so we know jQuery is working
+  console.log("asset: main.js is running with jquery");
+
+});
