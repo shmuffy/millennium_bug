e27f44f5d48f1c5da6d8205ad33b5a0627967fe9
Christian Kim <christiansactualbusiness@gmail.com>
2024-10-20 21:09:28 -0700

    Delete shmuffy directory

--- shmuffy/assets/js/main.js
+++
@@ -1,7 +0,0 @@
-// To be used for general functionality of the webpage
-$( document ).ready(function( ) {
-
-  // Just so we know jQuery is working
-  console.log("asset: main.js is running with jquery");
-
-});
